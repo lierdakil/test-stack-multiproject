99d096ae467b38fab0f8178b01fdb59e093bba20
Nikolay Yakimov <root@livid.pp.ru>
2017-02-26 14:34:15 +0300

    Initial commit

---
+++ a/src/LibA.hs
@@ -0,0 +1,10 @@
+module LibA where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+otherFunc :: IO ()
+otherFunc = putStrLn "otherFunc"
+
+-- thirdFunc :: IO ()
+-- thirdFunc = putStrLn "thirdFunc"
---
+++ b/app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import LibA
+
+main :: IO ()
+main = thirdFunc
